1dcc82b43267e566b43869649aef3f24845ef31e
ROHYUNSEOK <32957364+ROHYUNSEOK@users.noreply.github.com>
2020-02-04 14:49:52 +0900

    ROH:fix

--- project/socket_camera/socket_reverse/server_camera.py
+++ project/socket_camera/socket_reverse/server_camera.py
@@ -1,32 +1,3 @@
-Skip to content
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@ROHYUNSEOK 
-Learn Git and GitHub without any code!
-Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
-
-
-jiyeon037
-/
-project
-1
-00
- Code Issues 0 Pull requests 0 Actions Projects 0 Wiki Security Insights
-project/project/socket_camera/client_camera.py / 
-@ROHYUNSEOK ROHYUNSEOK ROH:test update
-a6253aa yesterday
-54 lines (43 sloc)  1.4 KB
-  
-Code navigation is available!
-Navigate your code with ease. Click on function and method calls to jump to their definitions or references in the same repository. Learn more
-
-You're using code navigation to jump to definitions or references.
-Learn more or give us feedback
 # -*- coding: utf-8 -*-
 # 소켓 통신으로 다중 웹캠 정보를 받아오는 파이썬 코드
 # 서버코드가 먼저 실행뒤에 클라이언트 코드를 실행할 것
@@ -61,11 +32,14 @@ def get_gpsdata():
     stringData = recvall(client_socket, int(length))
     print(stringData)
 
-HOST = '192.168.0.7'
+HOST = ''
 PORT = 9999
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
-client_socket.connect((HOST, PORT)) 
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((HOST, PORT)) 
+server_socket.listen() 
+print('server start')
 
 while True:    
     decimg1 = get_img_channel('1') # 1번 이미지 전송 요청
@@ -81,15 +55,3 @@ while True:
     if key == 27:
         break
 client_socket.close() 
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About

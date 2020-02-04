2d54ae1d7b865c8e704c040ee3111da9c563bc1b
ROHYUNSEOK <32957364+ROHYUNSEOK@users.noreply.github.com>
2020-02-04 13:05:04 +0900

    ROH:GUI fix...

--- project/GUI/imagetest.py
+++ project/GUI/imagetest.py
@@ -1,6 +1,6 @@
 #키보드 1 또는 2를 입력받아 이미지를 다르게 띄우는 프로그램
+# GUI 툴이 프로세스를 많이 잡아먹는거를 수정해야될 듯.....
 from tkinter import *
-
 from PIL import Image, ImageTk
 
 root = Tk()  # A root window for displaying objects
@@ -18,25 +18,49 @@ tkimage2 = ImageTk.PhotoImage(imageHead_original)
 
 data = 1
 
+panel1 = Label(root, image=tkimage)
+panel1.grid(row=3, column=3, sticky=E)
+
 def poll():
     global data
-    print("hello")
-    data = input()
-    data =int(data)
     print(data)
     if data == 1:
+        print("img1")
         panel1 = Label(root, image=tkimage)
         panel1.grid(row=3, column=3, sticky=E)
-        root.after(10, poll)
+        data= 3
+        root.after(1, LOOP)
 
     elif data == 2:
+        print("img2")
         panel1 = Label(root, image=tkimage2)
         panel1.grid(row=3, column=3, sticky=E)
-        root.after(10, poll)
+        data= 3
+        root.after(1, LOOP)
+        
+        
+    else:
+        root.after(1, LOOP)
 
+        
+def LOOP():
+    if True:
+        print("LOOP");
+        global data
+        data = int(input())
+        print(data)
+        if(data == 1 or data == 2):
+            print("goto poll")
+            root.after(1, poll)
+
+    ## loop문 시작
+        print("WAIT")
+    
 
     
-root.after(10, poll)
+    ## loop문 끝
+    
+root.after(1000, LOOP)
 root.mainloop()
         
         

f28d9416d52a202e6018d2bc23165f909eddb242
ROHYUNSEOK <32957364+ROHYUNSEOK@users.noreply.github.com>
2020-02-12 16:01:09 +0900

    ROH:donkey_assembly

--- project/Pi/car_streaming_joystick_with_PWM.py
+++ project/Pi/car_streaming_joystick_with_PWM.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 # 원격 동키카 제어 및 카메라 스트리밍 코드
 # 동키카는 흑백 영상을 Streaming
-# TEST VERSION
+'''
+2020.02.12
+동키카 PWM / STREAMING + 외부 연산장치 연산결과에 따른 GPIO 출력
+노면 상태가 젖은 경우 라즈베리파이 GPIO 21 번이 HIGH 상태가 되었다가 특정 시간이 지난 뒤에 자동으로 LOW됨
+
+
+'''
 import cv2
 import numpy
 import time
@@ -11,19 +17,29 @@ import time
 import RPi.GPIO as GPIO
 from _thread import *
 
+#라즈베리파이 GPIO
+import RPi.GPIO as GPIO
+GPIO_SIGNAL = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
+GPIO.output(GPIO_SIGNAL,False)
+
+
+
+
 import Adafruit_PCA9685
 pwm = Adafruit_PCA9685.PCA9685() #pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
 pwm.set_pwm_freq(60) # 서보모터 60Hz로 펄스주기를 설정.
 
 #### 동키카 PWM 펄스 조절 부분 #########
 # 이 부분의 값을 적절히 조절해서 전진/후진/정지/좌/우 조절할 것#
-PWM_GO   = 200
-PWM_BACK = 300
-PWM_STOP = 500
+PWM_GO   = 395
+PWM_BACK = 370
+PWM_STOP = 380
 
-PWM_LEFT   = 200
-PWM_RIGHT  = 300
-PWM_CENTER = 500
+PWM_LEFT = 260
+PWM_RIGHT  = 500
+PWM_CENTER = 380
 #### 동키카 PWM 펄스 조절 부분 #########
 
 
@@ -39,7 +55,7 @@ pygame.joystick.init()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
-HOST = '192.168.255.22'
+HOST = ''
 PORT = 9999
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
@@ -49,12 +65,17 @@ server_socket.listen()
 print('server start')
 
 
-global A,B
+A =b'hello'
+B =b'world'
 global GO
 global TILT
 global CONT_DATA
 GO = 0
-TILT = 0 
+TILT = 0
+tt =1
+
+ALARM = False
+init_time = time.time()
 
 #PCA9685 관련 펄스 초기설정 함수 
 def set_servo_pulse(channel, pulse):
@@ -97,13 +118,22 @@ def PygameHandler(events):
                 TILT = 0
                 
             return GO, TILT
-
-def grayscale(img): # 그레이스케일로 이미지 변환
+        
+# 그레이스케일로 이미지 변환
+def grayscale(img):
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
+## time.sleep의 system 독점을 방지하기 위한 내용
+def millis_python():
+    now_time = time.time()
+    now_time = (now_time - init_time)
+    return now_time
+
 # 쓰레드 함수  ( 소켓 통신 개시 이후 무한 loop 문 처럼 작동하는 구문 )
-def threaded(client_socket, addr): 
+def threaded(client_socket, addr):
+    global tt
     print('Connected by :', addr[0], ':', addr[1]) 
+    stringData = b''
     while True: 
         try:
             data = client_socket.recv(1024)
@@ -112,13 +142,16 @@ def threaded(client_socket, addr):
                 break
             ch_data = int(data)
             if ch_data == 1:
-                global A
                 stringData = A
                 #stringData = queue1.get()
             if ch_data == 2:
-                global B
                 stringData = B
                 #stringData = queue2.get()
+            if ch_data == 3:                           ### WARN SIGNAL
+                GPIO.output(GPIO_SIGNAL,False)
+                print("EEEE")
+                tt = millis_python()
+            
             client_socket.send(str(len(stringData)).ljust(16).encode())
             client_socket.send(stringData)
             ## 이 부분에 PWM 제어 신호 넣으면 됨
@@ -141,7 +174,11 @@ def threaded(client_socket, addr):
                 pwm.set_pwm(3, 0, PWM_RIGHT) #3번서보
             else:                      # 이 부분에 조향서보모터 중립
                 pwm.set_pwm(3, 0, PWM_CENTER) #3번서보   
-            
+
+            ## 특정 시간이 지나면 자동으로 알람을 해제하는 부분 ( 5초 이상 경과 시 OFF )
+            if (millis_python() - tt > 5):
+                GPIO.output(GPIO_SIGNAL,True)
+                ALARM = False
             
         except ConnectionResetError as e:
             print('Disconnected by ' + addr[0],':',addr[1])
@@ -150,7 +187,7 @@ def threaded(client_socket, addr):
 
 
 def webcam():
-    capture1 = cv2.VideoCapture(1) # 카메라 채널 바꿔주면 됨
+    capture1 = cv2.VideoCapture(0) # 카메라 채널 바꿔주면 됨
     capture2 = cv2.VideoCapture(2) # 카메라 채널 바꿔주면 됨
     while True:
         ret1, frame1 = capture1.read()
@@ -161,7 +198,6 @@ def webcam():
             result, imgencode = cv2.imencode('.jpg', frame1, encode_param)
             data1 = numpy.array(imgencode)
             stringData1 = data1.tostring()
-            #queue1.put(stringData1)
             global A
             A = stringData1
             cv2.imshow('CH1', frame1)
@@ -172,7 +208,6 @@ def webcam():
             result, imgencode = cv2.imencode('.jpg', frame2, encode_param)
             data2 = numpy.array(imgencode)
             stringData2 = data2.tostring()
-            #queue2.put(stringData2)
             global B
             B = stringData2
             cv2.imshow('CH2', frame2)
@@ -180,6 +215,7 @@ def webcam():
         if key == 27:
             break
 
+GPIO.output(GPIO_SIGNAL,True)
 
 start_new_thread(webcam, ())
         
@@ -189,6 +225,6 @@ while True:
     start_new_thread(threaded, (client_socket, addr )) 
 
 server_socket.close() 
-        
+GPIO.cleanup()        
                 
-                
+
--- project/Pi/client_camera.py
+++ project/Pi/client_camera.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 # 소켓 통신으로 다중 웹캠 정보를 받아오는 파이썬 코드
 # 서버코드가 먼저 실행뒤에 클라이언트 코드를 실행할 것
-
-
- 
+# GPS 추가 
 import socket 
 import numpy as np
 import cv2
-import time
+
 
 def recvall(sock, count):
     buf = b''
@@ -27,31 +25,29 @@ def get_img_channel(channel):
     img = cv2.imdecode(data,1)
     return img
 
+def get_gpsdata():
+    message = '3'
+    client_socket.send(message.encode()) ## 1번 이미지 전송 요청
+    length = recvall(client_socket,16)
+    stringData = recvall(client_socket, int(length))
+    print(stringData)
 
-HOST = '192.168.0.9'
+HOST = '192.168.0.5'
 PORT = 9999
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
 client_socket.connect((HOST, PORT)) 
 
 
-while True:
-    try:
-        #decimg1 = get_img_channel('1') # 1번 이미지 전송 요청
-        #cv2.imshow('CH1 CAM',decimg1)
+while True:    
+    decimg1 = get_img_channel('1') # 1번 이미지 전송 요청
+    cv2.imshow('CH1 CAM',decimg1)
 
-        #decimg2 = get_img_channel('2') # 2번 이미지 전송 요청
-        #cv2.imshow('CH2 CAM',decimg2)
+    decimg2 = get_img_channel('2') # 2번 이미지 전송 요청
+    cv2.imshow('CH2 CAM',decimg2)
 
-        #/get_gpsdata()
-        Message = '4'
-        client_socket.send(Message.encode())       
-        
-        key = cv2.waitKey(1)
-        if key == 27:
-            break
-        
-    except KeyboardInterrupt:
-        for i in range(5):
-            Message = '3'
-            client_socket.send(Message.encode()) ##알람 경보 활성화  
-            print("send")
+    #/get_gpsdata()
+   
+    
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
